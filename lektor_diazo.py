7b71d95a055292f722e9b9c4f3cf7e48c153595b
Alberto Berti <alberto@metapensiero.it>
2016-08-13 00:45:58 +0200

    First cut of the working Diazo theme transformer

--- lektor_diazo.py
+++ lektor_diazo.py
@@ -10,17 +10,81 @@ from __future__ import unicode_literals, absolute_import, print_function
 
 from codecs import open
 import os
+import re
 
+import click
+from diazo.compiler import compile_theme
 from lektor.pluginsystem import Plugin
+from lxml import etree, html
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     README = f.read()
+
+doctype_re= re.compile(b"^<!DOCTYPE\\s[^>]+>\\s*", re.MULTILINE)
 
 
 class DiazoThemePlugin(Plugin):
     name = 'Diazo themes for Lektor'
     description = README
 
+    charset = 'UTF-8'
+    doctype = '<!DOCTYPE html>'
+    file_encoding = 'utf-8'
+    ignored_extensions=(
+        'js', 'css', 'gif', 'jpg', 'jpeg', 'pdf', 'ps', 'doc',
+        'png', 'ico', 'mov', 'mpg', 'mpeg', 'mp3', 'm4a', 'txt',
+        'rtf', 'swf', 'wav', 'zip', 'wmv', 'ppt', 'gz', 'tgz',
+        'jar', 'xls', 'bmp', 'tif', 'tga', 'hqx', 'avi')
+    transform_permissions = {
+        'read_file': True,
+        'write_file': False,
+        'create_dir': False,
+        'read_network': False,
+        'write_network': False
+    }
+
+    def _parse_file(self, file_, base_url=None):
+        """Parse a file given a file/filename and returns an etree."""
+        return html.parse(file_, base_url=base_url)
+
+    def _serialize_etree(self, tree, fname):
+        """Save an etree into a file."""
+        assert isinstance(tree, etree._XSLTResultTree)
+        res = bytes(tree)
+
+        if self.doctype is not None:
+            doc_encoded = self.doctype.encode()
+            res, subs = doctype_re.subn(doc_encoded, res, 1)
+            if not subs:
+                res = doc_encoded + res
+        with open(fname, 'w') as f:
+            f.write(res)
+
+    def on_setup_env(self, **extra):
+        self.access_control = etree.XSLTAccessControl(**self.transform_permissions)
+        rules_path = os.path.join(self.env.root_path, 'theme', 'rules.xml')
+
+        if os.path.exists(rules_path):
+            tree = compile_theme(rules_path, access_control=self.access_control)
+            self.transform = etree.XSLT(tree,
+                                        access_control=self.access_control)
+        else:
+            self.transform = None # disabled
+
+        self.ignored_pattern = re.compile(
+            "^.*\.(%s)$" % '|'.join(self.ignored_extensions))
+
+
     def on_after_build(self, builder, build_state, source, prog, **extra):
-        print('after-build called')
+        if not self.transform:
+            return
+        sign = click.style('T', fg='blue')
+        for art in build_state.updated_artifacts:
+            if self.ignored_pattern.search(art.dst_filename) is None:
+                with open(art.dst_filename, encoding=self.file_encoding) as f:
+                    tree = self._parse_file(f)
+                res_tree = self.transform(tree)
+                self._serialize_etree(res_tree, art.dst_filename)
+                text = '%s %s' % (sign, art.artifact_name)
+                click.echo(text)
